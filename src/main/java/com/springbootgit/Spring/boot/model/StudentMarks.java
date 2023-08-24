443de08ad64344a037b312e1b43e9bb8a4f60a32
AshokVenkatachalam <ashokvenkat2001@gmail.com>
2023-08-24 11:51:22 +0530

    updated

--- src/main/java/com/springbootgit/Spring/boot/model/StudentMarks.java
+++ src/main/java/com/springbootgit/Spring/boot/model/StudentMarks.java
@@ -1,6 +1,5 @@
 package com.springbootgit.Spring.boot.model;
 
-import javax.annotation.Generated;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
